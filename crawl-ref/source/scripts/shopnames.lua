c51bd25322c535db480fb4eef9e2c65bf38db54a
mumra <pete.hurst@gmail.com>
2024-06-04 21:36:24 +0100

    Shop names: loads more refinement

--- crawl-ref/source/scripts/shopnames.lua
+++ crawl-ref/source/scripts/shopnames.lua
@@ -1,3 +1,9 @@
+-- Generates a huge number of shop names to test the generator
+-- Runs through the full range of "levels" (we increment 2 levels per dungeon
+-- level, so 54), all shop types, and again with Gozag shops
+-- Usage:
+-- ./crawl -script shopnames 1> shopnames_test.txt
+
 local shop_types = {
     "general",
     "antiques",
