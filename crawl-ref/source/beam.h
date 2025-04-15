35a452a67b6a7d5b06ddfebd02dd2b7df9bca8e1
mumra <pete.hurst@gmail.com>
2025-04-15 20:46:22 +0100

    multi-bolt: fix compile warning

--- crawl-ref/source/beam.h
+++ crawl-ref/source/beam.h
@@ -135,6 +135,7 @@ struct targeting_tracer : beam_tracer
 struct bolt
 {
     bolt();
+    virtual ~bolt() = default;
 
     // INPUT parameters set by caller
     spell_type  origin_spell = SPELL_NO_SPELL; // may remain SPELL_NO_SPELL for
