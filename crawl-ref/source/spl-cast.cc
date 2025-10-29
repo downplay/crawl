18cd9661ad3a58cb449abc341109df529d5e5fe3
mumra <pete.hurst@gmail.com>
2025-10-29 22:00:29 +0000

    fix mp cost after rebase (prob also from ungoldify)

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -1066,7 +1066,7 @@ spret cast_a_spell(bool check_range, spell_type spell, dist *_target,
     _handle_energy_orb(mp_cost, cast_result);
     if (cast_result == spret::success)
     {
-        stardust_orb_trigger(cost);
+        stardust_orb_trigger(mp_cost);
         if (you.unrand_equipped(UNRAND_MAJIN) && one_chance_in(500))
             _majin_speak(spell);
         did_god_conduct(DID_SPELL_CASTING, 1 + random2(5));
